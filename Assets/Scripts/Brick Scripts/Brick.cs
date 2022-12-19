5b5e5696e3749da3f0e4b378db565a9e868b8176
DESKTOP-UFGH3NC\ELİF <batuhan.altinel@hotmail.com>
2022-12-19 15:59:08 +0300

    Object Pooling implemented

--- Assets/Scripts/Brick Scripts/Brick.cs
+++ Assets/Scripts/Brick Scripts/Brick.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     ColorEnum colorEnum;
+    float xIndex;
+    float zIndex;
     // Start is called before the first frame update
     void Start()
     {
         colorEnum = GetComponent<ColorEnum>();
     }
 
+    public void InitPosition(float x, float z)
+    {
+       xIndex = x;
+       zIndex = z;
+    }
     void OnTriggerEnter(Collider other)
     {
-        if(this.colorEnum.colorType == other.GetComponent<ColorEnum>().colorType)
+        if(other.gameObject.TryGetComponent<ColorEnum>(out colorEnum) && gameObject.TryGetComponent<ColorEnum>(out colorEnum))
         {
-            Debug.Log("Colors matched Color name :" + colorEnum.colorType);
+            if(this.colorEnum.colorType == other.GetComponent<ColorEnum>().colorType)
+            {
+                Debug.Log("Colors matched Color name :" + colorEnum.colorType);
+                gameObject.SetActive(false);
+            }
         }
+        
     }
 }
---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+
+    private Queue bricksQueue = new();
+    public GameObject[] Bricks;
+    int amountOfObject;
+    private GameObject brickInstantiate;
+
+    List<Vector3> possiblePositions = new();
+    int maxX = 13;
+    int maxZ = 14;
+
+    public int xBorder = 10;
+    public int zBorder = 15;
+
+    void Start()
+    {
+        amountOfObject = (xBorder*zBorder) / Bricks.Length;
+        FirstBricksSpawn();
+        PossiblePositions();
+        SetBricksPositions();
+    }
+
+    
+    void FirstBricksSpawn()
+    {
+        for (int i = 0; i < Bricks.Length; i++)
+        {
+            for (int j = 0; j < amountOfObject; j++)
+            {
+                brickInstantiate = Instantiate(Bricks[i],transform.position,Quaternion.identity);
+                bricksQueue.Enqueue(brickInstantiate);
+                brickInstantiate.transform.parent = transform;
+                brickInstantiate.SetActive(false);
+            }
+        }
+    }
+
+    GameObject GetBrickFromPool()
+    {
+        foreach (GameObject brick in bricksQueue)
+        {
+            if(!brick.activeInHierarchy)
+            {
+                return brick;
+            }
+        }
+        return null;
+    }
+
+    List<Vector3> PossiblePositions()
+    {
+        for (int i = 0; i < zBorder; i++)
+        {
+            for (int j = 0; j < xBorder; j++)
+            {
+                Vector3 newPos = new Vector3(maxX,0.2f,maxZ);
+                possiblePositions.Add(newPos);
+                maxX -= 3;
+            }
+            maxZ -=2;
+            maxX = 13;
+        }
+        Debug.Log("Possible positions count " + possiblePositions.Count);
+        return possiblePositions;
+    }
+
+    Vector3 GetRandomPosition()
+    {
+        int randomPos = Random.Range(0,possiblePositions.Count);
+        Vector3 newPos = possiblePositions[randomPos];
+        possiblePositions.Remove(possiblePositions[randomPos]);
+        return newPos;
+    }
+
+    void SetBricksPositions()
+    {
+         for (int i = 0; i < Bricks.Length; i++)
+        {
+            for (int j = 0; j < amountOfObject; j++)
+            {
+                GameObject brick = GetBrickFromPool();
+                brick.SetActive(true);
+                brick.transform.position = GetRandomPosition();
+                // brick.GetComponent<Brick>().InitPosition(GetRandomPosition().x,GetRandomPosition().z);
+                bricksQueue.Dequeue();
+            }
+        }
+    }
+     
+    
+}
